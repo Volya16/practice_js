7f0471d20834a8c675dd5938752742830408af68
VitaliiZvieriev <zverew91@gmail.com>
2024-11-23 16:59:17 +0200

    add phonebook

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -133,32 +133,133 @@
 // raise() возводить першу властивсть в ступінь другої і повертає результат (з перевіркою на наявність властивостей в об'єкті)
 // * винеси перевірку на наявність властивостей в об'єкті в окремий метод exist ()
 
-const calculator = {
-  read(a, b) {
-    this.valueA = a;
-    this.valueB = b;
-  },
-  sum() {
-    if (this.exist()) {
-      return this.valueA + this.valueB;
+// const calculator = {
+//   read(a, b) {
+//     this.valueA = a;
+//     this.valueB = b;
+//   },
+//   sum() {
+//     if (this.exist()) {
+//       return this.valueA + this.valueB;
+//     }
+//     return "not values";
+//   },
+//   mult() {
+//     if (this.exist()) {
+//       return this.valueA * this.valueB;
+//     }
+//     return "not values";
+//   },
+//   raise() {
+//     if (this.exist()) {
+//       return this.valueA ** this.valueB;
+//     }
+//     return "not values";
+//   },
+//   exist() {
+//     return this.valueA && this.valueB;
+//   },
+// };
+
+// console.log(calculator.sum(), calculator.mult(), calculator.raise());
+
+//  10. Створіть телефонну книгу - об'єкт phonebook,
+// у якого є властивість contacts (список контактів)
+// та методи управління книгою:
+// add(data) - приймає об'єкт data, де зберігається
+// name, email, category, id, createdAt
+// (name i email - обов'язкові параметри, які треба передавати
+// при додаванні нового контакта,
+// category - може передаватись чи ні, якщо ні - має
+// приймати значення "default",
+// id та createdAt генеруються відповідними методами:
+// generateId() і getDate());
+
+// list() - виводить список контактів у вигляді таблиці;
+// filtered(category) - фільтрує контактів по обраній категорії (друзі, робота і т.д.)
+// delete(name) - видаляє контакт з заданим ім'ям;
+// updateName(oldName, newName) - змінює ім'я контакта;
+
+// * не забудь додати перевірку, якщо контакт з таким ім'ям чи імейлом вже є - ми його не додаємо
+
+const phonebook = {
+  contacts: [],
+  add(data) {
+    for (const contact of this.contacts) {
+      if (contact.name === data.name || contact.email === data.email) {
+        return;
+      }
     }
-    return "not values";
+
+    const newContact = {
+      name: data.name,
+      email: data.email,
+      category: data.category || "default",
+      id: this.generateId(),
+      createdAt: this.getDate()
+    };
+    this.contacts.push(newContact);
   },
-  mult() {
-    if (this.exist()) {
-      return this.valueA * this.valueB;
+  list() {
+    console.table(this.contacts);
+  },
+  filtered(category) {
+    let resArr = [];
+    for (const contact of this.contacts) {
+      if (contact.category === category) {
+        resArr.push(contact);
+      }
     }
-    return "not values";
+    return resArr;
   },
-  raise() {
-    if (this.exist()) {
-      return this.valueA ** this.valueB;
+  delete(name) {
+    for (let i = 0; i < this.contacts.length; i++) {
+      if (this.contacts[i].name === name) {
+        this.contacts.splice(i, 1);
+      }
     }
-    return "not values";
   },
-  exist() {
-    return this.valueA && this.valueB;
+  updateName(oldName, newName) {
+    for (const contact of this.contacts) {
+      if (contact.name === oldName) {
+        contact.name = newName;
+      }
+    }
   },
+
+  generateId() {
+    return "#" + Math.random().toString(36).substr(2, 9);
+  },
+  getDate() {
+    return Date.now();
+  }
 };
 
-console.log(calculator.sum(), calculator.mult(), calculator.raise());
+phonebook.add({
+  name: "Mango",
+  email: "mango@mail.com",
+  category: "friends"
+});
+
+phonebook.list();
+
+phonebook.add({
+  name: "Poly",
+  email: "poly@hotmail.com"
+});
+
+phonebook.list();
+phonebook.add({
+  name: "Katy",
+  email: "katy@hotmail.com",
+  category: "friends"
+});
+phonebook.list();
+
+console.log(phonebook.filtered("default"));
+
+phonebook.delete("Poly");
+phonebook.list();
+
+phonebook.updateName("Mango", "Poly");
+phonebook.list();
