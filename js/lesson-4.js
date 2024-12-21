77f1b0fe4ffe2b1ff57ac1ea9ea56e3ac4d3c86b
VitaliiZvieriev <zverew91@gmail.com>
2024-12-21 20:54:28 +0200

    add result

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -4,7 +4,6 @@
 // Парним лі вказати жовтий фон, непарним синій
 // Використовуй createElement
 
-
 // const list = document.createElement('ul');
 // const addBtn = document.createElement('button');
 // const removeBtn = document.createElement("button");
@@ -40,7 +39,6 @@
 //     }
 //     lastListItem.remove();
 // });
-
 
 // 2. Створити розмітку на основі масива даних, де у кожного елемента списку
 // буде вказано ім'я, кількість лайків і перелічені теги
@@ -93,9 +91,7 @@
 //   },
 // ];
 
-
 // const list = document.querySelector(".stats");
-
 
 // const listItem = tweets.map(
 //   ({ name, likes, tags, gender }) => `<li class="stats-item ${gender}">
@@ -105,6 +101,107 @@
 //       </li>`
 // ).join("");
 
-// list.innerHTML = listItem; 
+// list.innerHTML = listItem;
 
+// 3. Натиснувши кнопку "Подвоювати", збільшити значення
+// у кожному елементі списку у 2 рази
 
+// const btnEl = document.querySelector("#double");
+// const listItemsEls = document.querySelectorAll(".listItem");
+
+// btnEl.addEventListener("click", () => {
+//   listItemsEls.forEach((el) => (el.textContent *= 2));
+// });
+
+// 4.
+//  При натисканні на будь-який рядок у табличці відобразіть
+//  повідомлення з назвою продукту та його ціною.
+//  "Ви вибрали <product> за <price>".
+
+// const tbodyEl = document.querySelector("#productTable tbody");
+// const prodDetailEl = document.querySelector("#productDetails");
+
+// tbodyEl.addEventListener("click", (evt) => {
+//   if (evt.target.nodeName !== "TD") {
+//     return;
+//   }
+
+//   const [product, price] = evt.target.parentElement.children;
+//   prodDetailEl.textContent = ` Ви вибрали ${product.textContent.toLowerCase()} за ${price.textContent.toLowerCase()}.`;
+// });
+
+// 5.
+// При натисканні на кожну з кнопок підсумовуються значення з data-атрибутів.
+// За натисканням на кнопку "Вивести результат" виводиться сума значення, а також статистика з
+// інформацією про те, яка кнопка була натиснута скільки разів.
+
+// const listBtnEls = document.querySelector(".statList");
+// const resBtnEl = document.querySelector("#resultButton");
+// const resDivEl = document.querySelector("#resultSection");
+
+// let result = 0;
+// let objResStatistic = {};
+
+// listBtnEls.addEventListener("click", (evt) => {
+//   if (!evt.target.classList.contains("calcButton")) {
+//     return;
+//   }
+//   result += Number(evt.target.dataset.number);
+
+//   const key = evt.target.textContent;
+
+//   objResStatistic[key]
+//     ? (objResStatistic[key] += 1)
+//     : (objResStatistic[key] = 1);
+// });
+
+// resBtnEl.addEventListener("click", () => {
+//   let resRow = `Total: ${result} <br/>`;
+
+//   const res = Object.entries(objResStatistic);
+
+//   for (const [key, value] of res) {
+//     resRow += ` ${key} - ${value} <br/>`;
+//   }
+
+//   resDivEl.innerHTML = resRow;
+
+//   objResStatistic = {};
+//   result = 0;
+// });
+
+// 6. Створити невелику гру
+// Спочатку на екрані з'являється якась фігура рандомного коліру в рандомному місті
+// Натискаючі на фігуру, вона змінює свою форму, колір, місце розташування
+
+// const forms = [
+//   "width: 100px; height: 100px; border-width: 1px;",
+//   "width: 100px; height: 100px; border-radius: 50%;",
+//   "width: 150px; height: 100px; border-width: 1px;",
+//   "width: 200px; height: 100px; border-radius: 100px / 50px; ",
+//   "width: 150px; height: 100px; transform: skew(20deg)"
+// ];
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// function randomither(max) {
+//   return Math.floor(Math.random() * max);
+// }
+
+// const getStyles = () => {
+//   randomEl.style.cssText = forms[randomither(forms.length)];
+//   randomEl.style.backgroundColor = getRandomHexColor();
+//   randomEl.style.position = "absolute";
+//   randomEl.style.top = `${randomither(90)}%`;
+//   randomEl.style.left = `${randomither(90)}%`;
+// };
+
+// const randomEl = document.createElement("div");
+
+// getStyles();
+
+// randomEl.addEventListener("click", getStyles);
+
+// document.querySelector("body").appendChild(randomEl);
